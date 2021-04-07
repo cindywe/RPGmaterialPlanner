3c7e7689866fe2a88898cec6c14ff89d67105a82
nawa-ton <nawaratt.ton@gmail.com>
2021-04-07 16:05:21 -0700

    added functionalities for checking diff between owned and required materials

--- planner.pl
+++ planner.pl
@@ -2,30 +2,14 @@
 
 
 ask(Ans) :-
-    write("Tell me which career you want to upgrade to OR ask your question: "), flush_output(current_output),
+    write("Ask your question: "), flush_output(current_output),
     nl,
     readln(Q),
     question(Q, Ans).
 
-
-% ----- Test cases -----
-% ?- ask(Ans).
-% Ask your question below: 
-% |: What materials required for upgrading to fighter?
-% Ans = ["Magic = 4", "Shield = 4", "Attack = 12"].
-
-
-% ?- ask(Ans).
-% Ask your question below: 
-% |: How many attack required for upgrading to fighter?
-% Ans = 12.
-
+    
 
 % ------------ QUESTIONS ------------- %
-question([X], Ans) :-
-	write("You need these for upgrading: "),
-	requires_for_upgrading(X, Ans).
-
 question(['What',materials,required,for,upgrading,to,X,?], Ans) :-
 	requires_for_upgrading(X, Ans).
 	
@@ -34,13 +18,88 @@ question(['How',many,Y,required,for,upgrading,to,X,?], Ans) :-
 	material_for_upgrade(career(X), material(type(Y), Ans)).
 
 
+question(['Can','I',upgrade,to,X,?], Ans) :-
+	check_can_upgrade(X, Ans).
+
+
+question(['How',many,Y,'I',still,need,for,upgrading,to,X,?], Ans) :-
+	check_material_diff(Y, X, Ans).
+
+
+
 
 % ------------ LOGICS ------------- %
 
 % requires_for_upgrading(C, MList) is true if materials M, S, A are required for upgrading to career C.
 requires_for_upgrading(C, MList) :-
 	upgrade(career(C), [material(type(magic), Magic), material(type(shield),Shield), material(type(attack),Attack)]),
-	string_concat('Magic = ', Magic, M),
-	string_concat('Shield = ', Shield, S),
-	string_concat('Attack = ', Attack, A),
+	string_concat('magic = ', Magic, M),
+	string_concat('shield = ', Shield, S),
+	string_concat('attack = ', Attack, A),
 	MList = [M, S, A].
+
+
+check_can_upgrade(X, Ans) :-
+    write("How many magic do you have?"), flush_output(current_output),
+    nl,
+    readln(M),
+    write("How many shield do you have?"), flush_output(current_output),
+    nl,
+    readln(S),
+    write("How many attack do you have?"), flush_output(current_output),
+    nl,
+    readln(A),
+    compute_all_materials_diff(X, M, S, A, Ans).
+
+
+compute_all_materials_diff(C, [MagicOwned], [ShieldOwned], [AttackOwned], Ans) :-
+	upgrade(career(C), [material(type(magic), MagicRequired), material(type(shield), ShieldRequired), material(type(attack), AttackRequired)]),
+	MagicDiff is MagicRequired - MagicOwned,
+	ShieldDiff is ShieldRequired - ShieldOwned,
+	AttackDiff is AttackRequired - AttackOwned,
+	generate_upgrade_check_msg(C, MagicDiff, ShieldDiff, AttackDiff, Ans).
+
+
+generate_upgrade_check_msg(Career, MagicDiff, ShieldDiff, AttackDiff, Ans) :- 
+(   (MagicDiff =< 0, ShieldDiff =< 0, AttackDiff =< 0) ->
+	string_concat('You have enough materials!!! You can now upgrade to ', Career, Msg),
+    write(Msg)
+;
+    write('You cannot upgrade now. Here are the numbers of materials you still require: '),
+    getNumRequiredMaterial(MagicDiff, magic, M),
+    getNumRequiredMaterial(ShieldDiff, shield, S),
+    getNumRequiredMaterial(AttackDiff, attack, A),
+	Ans = [M, S, A]
+).
+
+
+check_material_diff(Y, X, Ans) :-
+	string_concat('How many ', Y, Temp),
+	string_concat(Temp, ' do you have?', Msg),
+	write(Msg), flush_output(current_output),
+	nl,
+    readln(N),
+    compute_single_material_diff(X, Y, [N], Ans).
+
+
+compute_single_material_diff(C, MaterialType, [MaterialOwned], Ans) :-
+	material_for_upgrade(career(C), material(type(MaterialType), MagicRequired)),
+	write('You still need: '),
+	Diff is MagicRequired - MaterialOwned,
+	getNumRequiredMaterial(Diff, MaterialType, Ans).
+
+
+
+% ----------- HELPER METHODS ---------- %
+
+getNumRequiredMaterial(Num, Material, Ans) :-
+(   Num > 0 ->
+	string_concat(Material, ' = ', Temp),
+	string_concat(Temp, Num, Ans)
+;
+    string_concat(Material, ' = - ', Ans)
+).
+
+
+
+
---
+++ test_cases.pl
@@ -0,0 +1,32 @@
+% ----- Test cases -----
+% ?- ask(Ans).
+% Ask your question below: 
+% |: What materials required for upgrading to fighter?
+% Ans = ["magic = 4", "shield = 4", "attack = 12"].
+
+
+% ?- ask(Ans).
+% Ask your question below: 
+% |: How many attack required for upgrading to fighter?
+% Ans = 12.
+
+% ?- ask(Ans).
+% Ask your question: 
+% |: Can I upgrade to mage?
+% How many magic do you have?
+% |: 5
+% How many shield do you have?
+% |: 3
+% How many attack do you have?
+% |: 6
+% You cannot upgrade now. Here are the numbers of materials you still require: 
+% Ans = ["magic = 9", "shield = - ", "attack = - "].
+
+
+% ?- ask(Ans).
+% Ask your question: 
+% |: How many attack I still need for upgrading to knight?
+% How many attack do you have?
+% |: 8
+% You still need: 
+% Ans = "attack = - ".
